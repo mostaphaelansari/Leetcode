0c9695b2c89aed10c46c6a6cf50fa841a3e6551f
Mostapha EL ANSARI <138169519+mostaphaelansari@users.noreply.github.com>
2025-01-18 17:28:10 +0100

    Time: 359 ms (43.29%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,23 +1,12 @@
--- WITH Processing_time AS (
---     SELECT 
---         machine_id,
---         process_id,
---         MAX(CASE WHEN activity_type = 'end' THEN timestamp END) -
---         MAX(CASE WHEN activity_type = 'start' THEN timestamp END) / 2 AS processing_time
---     FROM Activity
---     GROUP BY machine_id, process_id
--- )
--- SELECT machine_id , processing_time
--- FROM Processing_time
--- ORDER BY machine_id, process_id DESC;
-SELECT machine_id, 
-       ROUND(AVG(end_time - start_time), 3) AS processing_time
-FROM (
-    SELECT machine_id, 
-           process_id, 
+With ProcessTimes as(
+    select machine_id,
+           process_id,
            MAX(CASE WHEN activity_type = 'start' THEN timestamp END) AS start_time,
            MAX(CASE WHEN activity_type = 'end' THEN timestamp END) AS end_time
-    FROM Activity
-    GROUP BY machine_id, process_id
-) AS ProcessTimes
+    From Activity
+    group by machine_id,process_id
+)
+SELECT machine_id, 
+       ROUND(AVG(end_time - start_time), 3) AS processing_time
+FROM ProcessTimes
 GROUP BY machine_id;
