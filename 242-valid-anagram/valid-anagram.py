7a8d8cad376b389f384a549f325285478271884c
Mostapha EL ANSARI <138169519+mostaphaelansari@users.noreply.github.com>
2025-02-23 21:59:01 +0100

    Time: 27 ms (27.42%) | Memory: 13.5 MB (40.02%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,13 +5,4 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(s) != len(t):
-            return False
-        
-        # Convert strings to lists of characters and sort them
-        s_sorted = sorted(list(s))
-        t_sorted = sorted(list(t))
-        
-        # Compare the sorted lists
-        return s_sorted == t_sorted
-        
+        return sorted(s) == sorted(t)
