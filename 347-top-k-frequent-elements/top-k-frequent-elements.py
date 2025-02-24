44c17cf7e9416d6619f4b5549c96bf6c18fe16fb
Mostapha EL ANSARI <138169519+mostaphaelansari@users.noreply.github.com>
2025-02-24 20:33:05 +0100

    Time: 339 ms (5.06%) | Memory: 21.3 MB (40.62%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -3,7 +3,7 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         dict_nums = Counter(nums)
         result = []
-        for num , freq in dict_nums.items() :
+        for num , freq in dict_nums.items() : 
             if freq >= sorted(dict_nums.values(), reverse=True)[k - 1]:
                 result.append(num)
         return result
