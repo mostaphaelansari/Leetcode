4904dd44a1973aa84e1ce78d691fda304fc97e06
Mostapha EL ANSARI <138169519+mostaphaelansari@users.noreply.github.com>
2025-04-17 23:09:46 +0200

    Time: 7 ms (76.05%) | Memory: 12.5 MB (52.16%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -4,6 +4,21 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        if x < 0:
+        if x < 0 or (x % 10 == 0 and x != 0):
             return False
-        return str(x) == str(x)[::-1]
+        reverserd_half = 0
+        while x > reverserd_half :
+            digit = x % 10 
+            x = x // 10 
+            reverserd_half = reverserd_half *10 + digit
+        return x == reverserd_half or x == reverserd_half//10
+
+# class Solution(object):
+#     def isPalindrome(self, x):
+#         """
+#         :type x: int
+#         :rtype: bool
+#         """
+#         if x < 0:
+#             return False
+#         return str(x) == str(x)[::-1]
