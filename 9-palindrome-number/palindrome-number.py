c5a59c499e33c19e8d280ae99886a073ac5d8beb
Mostapha EL ANSARI <138169519+mostaphaelansari@users.noreply.github.com>
2025-04-17 23:09:00 +0200

    Time: 4 ms (89.75%) | Memory: 12.5 MB (11.12%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -4,11 +4,6 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        if x < 0 or (x % 10 == 0 and x != 0):
+        if x < 0:
             return False
-        reverserd_half = 0
-        while x > reverserd_half :
-            digit = x % 10 
-            x = x // 10 
-            reverserd_half = reverserd_half *10 + digit
-        return x == reverserd_half or x == reverserd_half//10
+        return str(x) == str(x)[::-1]
