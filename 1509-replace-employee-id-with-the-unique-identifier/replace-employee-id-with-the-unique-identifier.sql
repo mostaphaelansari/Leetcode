ebe76ff10561d5368b4746a6ad4008d46eecf574
Mostapha EL ANSARI <138169519+mostaphaelansari@users.noreply.github.com>
2025-08-19 15:41:21 +0200

    Time: 1728 ms (7.55%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,3 +1,3 @@
-SELECT unique_id, name 
-FROM Employees 
-LEFT JOIN EmployeeUNI ON Employees.id = EmployeeUNI.id;
+SELECT e2.unique_id, e1.name FROM Employees e1
+LEFT JOIN EmployeeUNI e2 
+ON e2.id = e1.id;
